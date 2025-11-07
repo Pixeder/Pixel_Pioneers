18935bc86d25af9bef82e671a2c0d890ceff6c14
Vinay Kumar <vinaykumar222007@gamil.com>
2025-11-08 01:05:05 +0530

    basic structure of server and user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,100 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      required: true,
+      type: String,
+      unique: true,
+      trim: true,
+      index: true,
+      lowercase: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      sparse: true,
+      trim: true,
+    },
+    avatarURL: {
+      type: String,
+      default:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/800px-User-avatar.svg.png',
+    },
+    password: {
+      required: true,
+      type: String,
+      minlength: 8,
+    },
+    gender: {
+      required: true,
+      enum: ["male" , "female" , "others"],
+      lowercase: true,
+      optional: true,
+    },
+    refreshToken: {
+      type: String,
+      default: '',
+    },
+    accessToken: {
+      type: String,
+      default: '',
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: `${process.env.ACCESS_TOKEN_EXPIRES}`,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: `${process.env.REFRESH_TOKEN_EXPIRES}`,
+    }
+  );
+};
+
+export const User = model('User', userSchema);
